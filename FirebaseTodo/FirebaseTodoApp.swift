33cf77b9f2655cd4517d35279e9159531ca5f768
Paul Franco <mail@paulfran.co>
2021-03-24 05:10:21 -0700

    Initial Commit

---
+++ FirebaseTodo/FirebaseTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTodoApp.swift
+//  FirebaseTodo
+//
+//  Created by Paul Franco on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
